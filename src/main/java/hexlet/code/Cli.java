ff53901febdc38123696fbf5f559f22fcc425324
Daukaevt <mupbam1@gmail.com>
2022-08-28 15:44:22 +0300

    first line added

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -9,6 +9,7 @@ public class Cli {
      * System.in scanner per user dialog game.
      */
     public static void haveName() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello, " + sc.next() + "!");
