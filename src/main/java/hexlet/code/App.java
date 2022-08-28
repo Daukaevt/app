73eeab1e8592a7abeb9a008fe62c18dc4576bcc7
Daukaevt <mupbam1@gmail.com>
2022-08-28 18:58:47 +0300

    Even line added

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -7,12 +7,15 @@ public class App {
         System.out.println("""
                 Please enter the game number and press Enter.
                 1 - Greet
+                2 - Even
                 0 - Exit""");
         Scanner scan = new Scanner(System.in);
         var strScan = scan.next();
         System.out.println("Your choice: " + strScan.toString());
-        if (strScan.equals("1")) {
-            Cli.haveName();
+        switch (strScan) {
+            case "1": Cli.haveName(); break;
+            case "2": Even.askEvenOrNot(); break;
+            default: break;
         }
         scan.close();
     }
