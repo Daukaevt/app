ac203ae090caef0b12181c854cd833d8f5f645c0
Daukaevt <mupbam1@gmail.com>
2022-08-28 15:46:00 +0300

    choice logic added

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,19 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.haveName();
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit""");
+        Scanner scan = new Scanner(System.in);
+        var strScan = scan.next();
+        System.out.println("Your choice: " + strScan.toString());
+        if (strScan.equals("1")) {
+            Cli.haveName();
+        }
+        scan.close();
     }
 }
