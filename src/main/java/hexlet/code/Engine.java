2c8c387c3f8982fe14f2993b8dcea5ae319a5926
Daukaevt <mupbam1@gmail.com>
2022-08-31 18:05:33 +0300

    even game logic included

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -12,27 +12,61 @@ public class Engine {
      */
     private static int count = 0;
     /**
-     * Even game interactive dialog.
+     * init game rules question.
      */
-    public static void makeDialog() {
+    private static String gameQuestion = "";
+    /**
+     * Even game interactive dialog.
+     * @param inp user game choice.
+     */
+    @SuppressWarnings("checkstyle:AvoidNestedBlocks")
+    public static void makeDialog(final int inp) {
         //Engine.sayHello();
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!\n"
                 + "May I have your name?");
         var name = sc.next();
         System.out.println("Hello, " + name + "!");
-        System.out.println(Calc.askResultString()); // to add Even.String
+        var quest = "";
         for (int i = 0; i < MAXGAMES; i++) {
-            var quest = Calc.makeExpression();
-            System.out.println("Question:  " + quest); // to add Even.intRandom
-            if (sc.hasNextLine()) {
-                System.out.print("Your answer: "); // diferent from even
-                String inputAnswer = sc.next();
-                var check = Calc.checkAnswer(quest, inputAnswer);
-                if (CORRECT.equals(check)) {
-                    count++;
-                }
-                System.out.println(check);
+            switch (inp) {
+                case EVEN:
+                    gameQuestion = Even.askIfEvenString();
+                    if (i == 0) {
+                        System.out.println(gameQuestion); // to add Even.String
+                    }
+                    quest = String.valueOf(Even.intRandom());
+                    System.out.println("Question:  " + quest); // to add Even.intRandom
+                    if (sc.hasNextLine()) {
+                        String inputAnswer = sc.next();
+                        var check = Even.checkEven(Integer.parseInt(quest), inputAnswer);
+                        if (CORRECT.equals(check)) {
+                            count++;
+                        }
+                        System.out.println(check);
+                    }
+                    break;
+                case CALC:
+                    gameQuestion = Calc.askResultString();
+                    if (i == 0) {
+                        System.out.println(gameQuestion); // to add Even.String
+                    }
+                    quest = Calc.makeExpression();
+                    System.out.println("Question:  " + quest); // to add Even.intRandom
+                    if (sc.hasNextLine()) {
+                        System.out.print("Your answer: "); // diferent from even
+                        String inputAnswer = sc.next();
+                        var check = Calc.checkAnswer(quest, inputAnswer);
+                        if (CORRECT.equals(check)) {
+                            count++;
+                        } else {
+                            check += "\nLet's try again, " + name + "!";
+                        }
+                        System.out.println(check);
+                    }
+                    break;
+                default: throw new IllegalStateException(
+                        "Unexpected value from Engine");
             }
         }
         congratulate(name);
@@ -44,16 +78,6 @@ public class Engine {
             System.out.println(
                     "Congratulations, " + name + "!");
         }
-    }
-
-    /**
-     * random init.
-     * @return random int from 0 to 100
-     */
-
-    private static int intRandom() {
-        Random rnd = new Random(); //instance of random class
-        return rnd.nextInt(0, MAXRND);
     }
 
     /**
