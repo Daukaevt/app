34a64c48efb23ffd31123d977bedb0e731c9245e
Daukaevt <mupbam1@gmail.com>
2022-09-16 11:38:14 +0300

    logic remooved to game classes

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -2,144 +2,38 @@ package hexlet.code;
 
 
 
-import hexlet.code.games.*;
-
-import java.util.Objects;
-import java.util.Scanner;
-
-import static hexlet.code.CONST.*;
+import java.util.Random;
 
 public class Engine {
-    /**
-     * init current answers count.
+     /**
+     * ask game question.
+     * @param quest current game quest.
      */
-    private static int count = 0;
-
-    /**
-     * Even game interactive dialog.
-     * @param inp user game choice.
-     */
-    @SuppressWarnings("checkstyle:AvoidNestedBlocks")
-    public static void makeDialog(final int inp) {
-        //Engine.sayHello();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!\n"
-                + "May I have your name?");
-        var name = sc.next();
-        System.out.println("Hello, " + name + "!");
-        var quest = "";
-        for (int i = 0; i < MAXGAMES; i++) {
-            switch (inp) {
-                case EVEN:
-                    /*
-                      init game rules question.
-                     */
-                    String gameQuestion = Even.askIfEvenString();
-                    quest = String.valueOf(Even.intRandom());
-                    makeQuestion(i, gameQuestion, quest);
-                    if (sc.hasNextLine()) {
-                        String inputAnswer = sc.next();
-                        var check = Even.checkEven(
-                                Integer.parseInt(quest), inputAnswer);
-                        validation(check, "");
-                    }
-                    break;
-                case CALC:
-                    gameQuestion = Calc.askResultString();
-                    quest = Calc.makeExpression();
-                    makeQuestion(i, gameQuestion, quest);
-                    if (sc.hasNextLine()) {
-                        System.out.print("Your answer: "); // diferent from even
-                        String inputAnswer = sc.next();
-                        var check = Calc.checkAnswer(quest, inputAnswer);
-                        validation(check, name);
-                    }
-                    break;
-                case GCD:
-                    gameQuestion = hexlet.code.games.GCD.askFindGCD();
-                    quest = hexlet.code.games.GCD.findGCD();
-                    makeQuestion(i, gameQuestion, quest);
-                    if (sc.hasNextLine()) {
-                        System.out.print("Your answer: "); // diferent from even
-                        String inputAnswer = sc.next();
-                        String check = hexlet.code.games
-                                .GCD.checkAnswer(quest, inputAnswer);
-                        validation(check, name);
-                    }
-                    break;
-                case PROGRESSION:
-                    gameQuestion = Progression.askMissingNumber();
-                    quest = hexlet.code.games
-                            .Progression.findMissingNumberInRange();
-                    makeQuestion(i, gameQuestion, quest);
-                    if (sc.hasNextLine()) {
-                        System.out.print("Your answer: ");
-                        String inputAnswer = sc.next();
-                        String check = hexlet.code.games
-                                .Progression.checkAnswer(quest, inputAnswer);
-                        validation(check, name);
-                    }
-                    break;
-                case PRIME:
-                    gameQuestion = Prime.askIfPrime();
-                    quest = hexlet.code.games
-                            .Prime.getRndParams();
-                    makeQuestion(i, gameQuestion, quest);
-                    if (sc.hasNextLine()) {
-                        System.out.print("Your answer: ");
-                        String inputAnswer = sc.next();
-                        String check = hexlet.code.games
-                                .Prime.checkAnswer(quest, inputAnswer);
-                        validation(check, name);
-                    }
-                    break;
-                default: throw new IllegalStateException(
-                        "Unexpected value from Engine");
-            }
-        }
-        congratulate(name);
-        sc.close();
+    public static void makeQuest(final String quest) {
+        System.out.print("Question:  " + quest + "\nYour answer: ");
     }
-
     /**
-     * Validate if answer is correct.
-     * @param check if correct string.
-     * @param name user name string.
+     * gives a game quest answer.
+     * @param correct game correct answer.
      */
-    private static void validation(String check, final String name) {
-        if (CORRECT.equals(check)) {
-            count++;
-        } else {
-            if (!Objects.equals(name, "")) {
-                check += TRYAGAIN + name + EXCLAMATION_POINT;
-            }
-        }
-        System.out.println(check);
+    public static void checkInputAnswer(final String correct) {
+        System.out.println(correct);
     }
-
     /**
-     * ask uniq game question.
-     * @param i game loop number int.
-     * @param gameQuestion uniq game question string.
-     * @param quest params of game question.
+     * Even game if even question.
+     * @param max max rnd number.
+     * @return if even question String.
      */
-    private static void makeQuestion(
-            final int i, final String gameQuestion, final String quest) {
-        if (i == 0) {
-            System.out.println(gameQuestion);
-        }
-        System.out.println("Question:  " + quest);
+    public static int random(final int max) {
+        Random rnd = new Random(); //instance of random class
+        return rnd.nextInt(0, max);
     }
-
     /**
      * game points counter.
      * @param name user name string.
      */
-    private static void congratulate(final String name) {
-        if (count == WINCOUNT) {
+    public static void congratulate(final String name) {
             System.out.println(
                     "Congratulations, " + name + "!");
-        }
     }
-
 }
