e5380581eff0f44f4ce440b5032a02ef08e32f8c
Daukaevt <mupbam1@gmail.com>
2022-09-16 11:41:32 +0300

    game logic added

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -1,12 +1,56 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Arrays;
 
 import java.util.Random;
-
-import static hexlet.code.CONST.*;
+import java.util.Scanner;
+import static hexlet.code.games.Even.GAMES;
 
 public class GCD {
+    /**
+     * init correct answers count.
+     */
+    private static int count = 0;
+    /**
+     * correct string const.
+     */
+    public static final String CORRECT = "Correct!";
+    /**
+     * wrong string const.
+     */
+    public static final String WRONG =
+            "' is wrong answer ;(. Correct answer was '";
+    /**
+     * start GCD game logic.
+     * @param scName username.
+     */
+    public static void play(final String scName) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(askFindGCD());
+        for (int i = 0; i < GAMES; i++) {
+            var quest = makeExpression();
+            Engine.makeQuest(quest);
+            String userInput =  scanner.next();
+            String correct = checkUserInput(quest, userInput);
+            Engine.checkInputAnswer(correct);
+        }
+        if (count == GAMES) {
+            Engine.congratulate(scName);
+        }
+        scanner.close();
+    }
+    /**
+     * find gcd question params.
+     * @return params string.
+     */
+    private static String makeExpression() {
+        Random rnd = new Random(); //instance of random class
+        int firstRND = rnd.nextInt(0, Even.MAXRND);
+        int secondRND = rnd.nextInt(0, Even.MAXRND);
+        return firstRND + " " + secondRND;
+    }
 
     /**
      * GCD game find gcd question.
@@ -17,23 +61,12 @@ public class GCD {
     }
 
     /**
-     * find gcd question params.
-     * @return params string.
-     */
-    public static String findGCD() {
-        Random rnd = new Random(); //instance of random class
-        int firstRND = rnd.nextInt(0, MAXRND);
-        int secondRND = rnd.nextInt(0, MAXRND);
-        return firstRND + " " + secondRND;
-    }
-
-    /**
      * check if user input answer is correct.
      * @param quest find gcd question params.
      * @param inputAnswer user input answer.
      * @return correct/incorrect string.
      */
-    public static String checkAnswer(
+    public static String checkUserInput(
             final String quest, final String inputAnswer) {
         String[] str = quest.split(" ");
         var firstInt = Integer.parseInt(str[0]);
@@ -49,6 +82,7 @@ public class GCD {
         String incorrectStr = "'" + inputAnswer
                 + WRONG + arr[1] + "'";
         if (String.valueOf(arr[1]).equals(inputAnswer)) {
+            count++;
             return CORRECT;
         }
         return incorrectStr;
